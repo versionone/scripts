62f6ddb2c4ceae419f4db4744681cbd43efa202b
rajiv <rajiv.delwadia@versionone.com>
2010-12-23 12:20:03 -0500

    emit confirmation text

--- reset-passwords/reset-password-admin.sql
+++ reset-passwords/reset-password-admin.sql
@@ -6,15 +6,16 @@
  */
 declare @saveChanges bit; --set @saveChanges = 1
 
+declare @error int, @rowcount varchar(20)
 set nocount on; begin tran; save tran TX
-
 
 update Login 
 set PasswordHash=0x0437373325976655A750627948DEB21B5CD954764A  -- 'admin'
 where ID=20
 
-if @@ERROR<>0 goto ERR
-
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' password(s) reset'
 
 if (@saveChanges = 1) goto OK
 raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
--- reset-passwords/reset-password-password.sql
+++ reset-passwords/reset-password-password.sql
@@ -1,4 +1,3 @@
-
 /*	
  *	Reset all user's passwords to 'password'
  *	
@@ -7,14 +6,15 @@
  */
 declare @saveChanges bit; --set @saveChanges = 1
 
+declare @error int, @rowcount varchar(20)
 set nocount on; begin tran; save tran TX
-
 
 update Login 
 set PasswordHash=0x04104992C230B55875FA684E9FC8237B32837E1C35 -- 'password'
 
-if @@ERROR<>0 goto ERR
-
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' passwords reset'
 
 if (@saveChanges = 1) goto OK
 raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
