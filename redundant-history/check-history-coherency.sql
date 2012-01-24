3f6d0c5107a0d3e5765ecc5e93d0b4fb8f29d8c0
rajiv <rajiv.delwadia@versionone.com>
2012-01-24 14:34:47 -0500

    S-25458 scripts for checking history coherency and history vs now

---
+++ redundant-history/check-history-coherency.sql
@@ -0,0 +1,41 @@
+set nocount on
+create table #results (Asset sysname not null, [ID-Prior] int, [AuditBegin-Prior] int, [AuditEnd-Prior] int, [Sequence-Prior] int, [ID-Next] int, [AuditBegin-Next] int, [AuditEnd-Next] int, [Sequence-Next] int)
+declare @now sysname, @hist sysname, @andColsEqual varchar(max)
+declare C cursor local fast_forward for
+
+	select H.TABLE_NAME hist
+	from INFORMATION_SCHEMA.TABLES H 
+	where H.TABLE_TYPE<>'VIEW'
+		and 4=(select count(*) from INFORMATION_SCHEMA.COLUMNS C where C.TABLE_NAME=H.TABLE_NAME and COLUMN_NAME in ('ID','AssetType','AuditBegin','AuditEnd'))
+
+open C
+while 1=1 begin
+	fetch next from C into @hist
+	if @@FETCH_STATUS<>0 break
+
+	declare @sql varchar(max); select @sql = '
+		;with H as (
+			select ID, AuditBegin, AuditEnd, R=ROW_NUMBER() over(partition by ID order by AuditBegin)
+			from dbo.[{hist}]
+		)
+		insert #results
+		select ''{hist}'' Asset, *
+		from H A left join H B on A.ID=B.ID and A.R+1=B.R
+		where isnull(A.AuditEnd, -1)<>isnull(B.AuditBegin, -1)'
+
+	select @sql = REPLACE(@sql, '{hist}', @hist)
+
+	--print(@sql)
+	exec(@sql)
+
+end
+close C; deallocate c
+
+
+if 0<(select count(*) from #results) begin
+	raiserror('History is not coherent! See results.',16,1)
+	select * from #results
+end else
+	print 'History is coherent'
+	
+drop table #results
---
+++ redundant-history/check-history-vs-now.sql
@@ -0,0 +1,49 @@
+set nocount on
+create table #results (Asset sysname not null, [History ID] int, [History AuditBegin] int, [Now ID] int)
+declare @now sysname, @hist sysname, @andColsEqual varchar(max)
+declare C cursor local fast_forward for
+
+	select N.TABLE_NAME nowTable, H.TABLE_NAME histTable, 
+		andColsEqual = 
+			(
+				select REPLACE(' and (H.{col}=N.{col} or (H.{col} is null and N.{col} is null))', '{col}', quotename(COLUMN_NAME))
+				from INFORMATION_SCHEMA.COLUMNS C 
+				where C.TABLE_NAME=H.TABLE_NAME
+					and COLUMN_NAME not in ('ID','AssetType','AuditBegin','AuditEnd')
+				for xml path('')
+			)
+	from INFORMATION_SCHEMA.TABLES N
+	join INFORMATION_SCHEMA.TABLES H on H.TABLE_NAME+'_Now' = N.TABLE_NAME
+	where N.TABLE_TYPE<>'VIEW' and H.TABLE_TYPE<>'VIEW'
+
+open C
+while 1=1 begin
+	fetch next from C into @now, @hist, @andColsEqual
+	if @@FETCH_STATUS<>0 break
+
+	declare @sql varchar(max); select @sql = '
+		insert #results(Asset, [History ID], [History AuditBegin], [Now ID])
+		select ''{hist}'', H.ID, H.AuditBegin, N.ID
+		from dbo.[{hist}] H 
+		full outer join dbo.[{now}] N on H.ID=N.ID and H.AuditBegin=N.AuditBegin
+			{andColsEqual}
+		where (H.ID is null or N.ID is null) and H.AuditEnd is null'
+
+	select @sql = REPLACE(@sql, '{now}', @now)
+	select @sql = REPLACE(@sql, '{hist}', @hist)
+	select @sql = REPLACE(@sql, '{andColsEqual}', @andColsEqual)
+
+	--print(@sql)
+	exec(@sql)
+
+end
+close C; deallocate c
+
+
+if 0<(select count(*) from #results) begin
+	raiserror('"Now" is not consistent with "history"! See results.',16,1)
+	select * from #results
+end else
+	print '"Now" is consistent with "history"'
+	
+drop table #results
