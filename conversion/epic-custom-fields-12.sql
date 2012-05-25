89b70df564724a7911b7fb4e7ae9c7a698648d63
rajiv <rajiv.delwadia@versionone.com>
2012-05-25 16:38:20 -0400

    integrate converted data with newly-entered data, and re-stitch the histories

--- conversion/epic-custom-fields-12.sql
+++ conversion/epic-custom-fields-12.sql
@@ -81,63 +81,123 @@ select @rowcount=@@ROWCOUNT, @error=@@ERROR
 if @error<>0 goto ERR
 
 if (@storyAttributeType='Boolean' and @epicAttributeType='Boolean') begin
-	if exists(select * from dbo.CustomBoolean where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,5, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomBoolean(Definition, ID, AuditBegin, AuditEnd, Value)
 	select @epicDefinition, EpicID, AuditBegin, AuditEnd, Value
 	from @map Map
 	join dbo.CustomBoolean on Definition=@storyDefinition and ID=StoryID
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+
+	;with A as (
+		select Definition, ID, AuditBegin, AuditEnd, Value, R=row_number() over(partition by Definition, ID order by AuditBegin)
+		from dbo.CustomBoolean
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.ID=B.ID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
 end
 
 else if (@storyAttributeType='Date' and @epicAttributeType='Date') begin
-	if exists(select * from dbo.CustomDate where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,6, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomDate(Definition, ID, AuditBegin, AuditEnd, Value)
 	select @epicDefinition, EpicID, AuditBegin, AuditEnd, Value
 	from @map Map
 	join dbo.CustomDate on Definition=@storyDefinition and ID=StoryID
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+
+	;with A as (
+		select Definition, ID, AuditBegin, AuditEnd, Value, R=row_number() over(partition by Definition, ID order by AuditBegin)
+		from dbo.CustomDate
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.ID=B.ID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
 end
 
 else if (@storyAttributeType='LongText' and @epicAttributeType='LongText') begin
-	if exists(select * from dbo.CustomLongText where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,7, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomLongText(Definition, ID, AuditBegin, AuditEnd, Value)
 	select @epicDefinition, EpicID, AuditBegin, AuditEnd, Value
 	from @map Map
 	join dbo.CustomLongText on Definition=@storyDefinition and ID=StoryID
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+
+	;with A as (
+		select Definition, ID, AuditBegin, AuditEnd, Value, R=row_number() over(partition by Definition, ID order by AuditBegin)
+		from dbo.CustomLongText
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.ID=B.ID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
 end
 
 else if (@storyAttributeType='Numeric' and @epicAttributeType='Numeric') begin
-	if exists(select * from dbo.CustomNumeric where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,8, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomNumeric(Definition, ID, AuditBegin, AuditEnd, Value)
 	select @epicDefinition, EpicID, AuditBegin, AuditEnd, Value
 	from @map Map
 	join dbo.CustomNumeric on Definition=@storyDefinition and ID=StoryID
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+
+	;with A as (
+		select Definition, ID, AuditBegin, AuditEnd, Value, R=row_number() over(partition by Definition, ID order by AuditBegin)
+		from dbo.CustomNumeric
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.ID=B.ID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
 end
 
 else if (@storyAttributeType='Text' and @epicAttributeType='Text') begin
-	if exists(select * from dbo.CustomText where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,9, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomText(Definition, ID, AuditBegin, AuditEnd, Value)
 	select @epicDefinition, EpicID, AuditBegin, AuditEnd, Value
 	from @map Map
 	join dbo.CustomText on Definition=@storyDefinition and ID=StoryID
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+
+	;with A as (
+		select Definition, ID, AuditBegin, AuditEnd, Value, R=row_number() over(partition by Definition, ID order by AuditBegin)
+		from dbo.CustomText
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.ID=B.ID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
 end
 
 else if (@storyAttributeType='Relation' and @epicAttributeType='Relation') begin
@@ -146,20 +206,28 @@ else if (@storyAttributeType='Relation' and @epicAttributeType='Relation') begin
 		goto ERR
 	end
 	
-	if exists(select * from dbo.CustomRelation where Definition=@epicDefinition) begin
-		raiserror('%s already contains data',16,10, @epicDefinition)
-		goto ERR
-	end
-	
 	insert dbo.CustomRelation(Definition, PrimaryID, ForeignID, AuditBegin, AuditEnd)
 	select @epicDefinition, EpicID, ForeignID, AuditBegin, AuditEnd
 	from @map Map
 	join dbo.CustomRelation on Definition=@storyDefinition and PrimaryID=StoryID
-end
+	
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
 
-select @rowcount=@@ROWCOUNT, @error=@@ERROR
-if @error<>0 goto ERR
-print @rowcount + ' records converted from ' + @storyDefinition + ' to ' + @epicDefinition
+	;with A as (
+		select Definition, PrimaryID, ForeignID, AuditBegin, AuditEnd, R=row_number() over(partition by Definition, PrimaryID, ForeignID order by AuditBegin)
+		from dbo.CustomRelation
+	)
+	update A
+	set AuditEnd=B.AuditBegin
+	from A B
+	where A.Definition=@epicDefinition and B.Definition=@epicDefinition and A.PrimaryID=B.PrimaryID and A.ForeignID=B.ForeignID and A.R+1=B.R and (A.AuditEnd is null or A.AuditEnd>B.AuditBegin)
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+	if @rowcount<>0 print @rowcount + ' conflicting records restitched'
+end
 
 if (@saveChanges = 1) goto OK
 raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
