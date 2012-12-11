62ef7a463030662e902456142b1a5db2d721505a
rajiv <rajiv.delwadia@versionone.com>
2012-12-11 17:01:58 -0500

    only count non-deleted workitem toward choosing a project's schedule

--- fixes/fix-missing-schedules.sql
+++ fixes/fix-missing-schedules.sql
@@ -27,30 +27,6 @@ END
 declare @error int, @rowcount varchar(20)
 set nocount on; begin tran; save tran TX
 
-/*
-select Scope_Now.ID, Scope_Now.ScheduleID, Y.ScheduleID
-from dbo.Scope_Now 
-, (
-	select distinct ScopeID, ScheduleID
-	from dbo.Workitem_Now
-	join dbo.Timebox_Now on Timebox_Now.ID=Workitem_Now.TimeboxID
-) Y
-where Scope_Now.ID=Y.ScopeID and (Scope_Now.ScheduleID is null or Scope_Now.ScheduleID<>Y.ScheduleID)
-
-select Scope.ID, Scope.AuditBegin, Scope.ScheduleID, Y.ScheduleID
-from dbo.Scope,
-(
-	select distinct Scope.ID, Scope.AuditBegin, Timebox.ScheduleID
-	from Workitem, Scope, Timebox
-	where Scope.ScheduleID is null
-		and Scope.ID=Workitem.ScopeID and Timebox.ID=Workitem.TimeboxID
-		and Workitem.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) 
-		and Timebox.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) and Workitem.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
-		and Timebox.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
-) Y
-where Scope.ID=Y.ID and Scope.AuditBegin=Y.AuditBegin and (Scope.ScheduleID is null or Scope.ScheduleID<>Y.ScheduleID)
-*/
-
 alter table Scope_Now disable trigger all
 alter table Scope disable trigger all
 
@@ -62,7 +38,10 @@ from (
 	select ScopeID, min(ScheduleID) ScheduleID
 	from dbo.Workitem_Now
 	join dbo.Timebox_Now on Timebox_Now.ID=Workitem_Now.TimeboxID
+	join dbo.BaseAsset_Now on BaseAsset_Now.ID=Workitem_Now.ID
+	where AssetState<255
 	group by ScopeID
+	--order by 1
 ) Y
 where Scope_Now.ID=Y.ScopeID and (Scope_Now.ScheduleID is null or Scope_Now.ScheduleID<>Y.ScheduleID)
 
@@ -76,11 +55,28 @@ print @rowcount + ' current scope schedules restored'
 update dbo.Scope set ScheduleID=Y.ScheduleID
 from (
 	select Scope.ID, Scope.AuditBegin, min(Timebox.ScheduleID) ScheduleID
-	from Workitem, Scope, Timebox
-	where Scope.ScheduleID is null
-		and Scope.ID=Workitem.ScopeID and Timebox.ID=Workitem.TimeboxID
-		and Workitem.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) 
-		and Timebox.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) and Workitem.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
+	from (select
+		AuditBegin=case 
+			when BaseAsset.AuditBegin<Workitem.AuditBegin then Workitem.AuditBegin 
+			else BaseAsset.AuditBegin 
+		end,
+		AuditEnd=case 
+			when BaseAsset.AuditEnd is null then Workitem.AuditEnd 
+			when Workitem.AuditEnd<BaseAsset.AuditEnd then Workitem.AuditEnd 
+			else BaseAsset.AuditEnd 
+		end,
+		ScopeID,
+		TimeboxID
+		from BaseAsset, Workitem
+		where
+			BaseAsset.ID=Workitem.ID
+			and AssetState<255
+			and BaseAsset.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) and Workitem.AuditBegin<isnull(BaseAsset.AuditEnd, 2147483647) 
+	) W, Scope, Timebox
+	where
+		Scope.ID=W.ScopeID and Timebox.ID=W.TimeboxID
+		and W.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(W.AuditEnd, 2147483647) 
+		and Timebox.AuditBegin<isnull(W.AuditEnd, 2147483647) and W.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
 		and Timebox.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
 	group by Scope.ID, Scope.AuditBegin
 ) Y
@@ -90,36 +86,12 @@ select @rowcount=@@ROWCOUNT, @error=@@ERROR
 if @error<>0 goto ERR
 print @rowcount + ' historical scope schedules restored'
 
-alter table Scope_Now enable trigger all
-alter table Scope enable trigger all
-
-/*
-select Scope_Now.ID, Scope_Now.ScheduleID, Y.ScheduleID
-from dbo.Scope_Now 
-, (
-	select distinct ScopeID, ScheduleID
-	from dbo.Workitem_Now
-	join dbo.Timebox_Now on Timebox_Now.ID=Workitem_Now.TimeboxID
-) Y
-where Scope_Now.ID=Y.ScopeID and (Scope_Now.ScheduleID is null or Scope_Now.ScheduleID<>Y.ScheduleID)
-
-select Scope.ID, Scope.AuditBegin, Scope.ScheduleID, Y.ScheduleID
-from dbo.Scope,
-(
-	select distinct Scope.ID, Scope.AuditBegin, Timebox.ScheduleID
-	from Workitem, Scope, Timebox
-	where Scope.ScheduleID is null
-		and Scope.ID=Workitem.ScopeID and Timebox.ID=Workitem.TimeboxID
-		and Workitem.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) 
-		and Timebox.AuditBegin<isnull(Workitem.AuditEnd, 2147483647) and Workitem.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
-		and Timebox.AuditBegin<isnull(Scope.AuditEnd, 2147483647) and Scope.AuditBegin<isnull(Timebox.AuditEnd, 2147483647) 
-) Y
-where Scope.ID=Y.ID and Scope.AuditBegin=Y.AuditBegin and (Scope.ScheduleID is null or Scope.ScheduleID<>Y.ScheduleID)
-*/
-
-
 if (@saveChanges = 1) goto OK
 raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
 ERR: rollback tran TX
 OK: commit
+
+alter table Scope_Now enable trigger all
+alter table Scope enable trigger all
+
 DONE:
