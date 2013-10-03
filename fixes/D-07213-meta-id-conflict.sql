305d844d204169908f32ecfa46ec4ececfd6c2b6
rajiv <rajiv.delwadia@versionone.com>
2013-10-03 13:48:09 -0400

    clean up meta ID conflict, as described in D-07213
    upgrading from 12.1.3 to 12.2 or later will leave a spurious business-rule in meta

---
+++ fixes/D-07213-meta-id-conflict.sql
@@ -0,0 +1,38 @@
+/*	
+ *	Fix Meta ID conflict, as identified in D-07213
+ *	
+ *	NOTE:  This script defaults to rolling back changes.
+ *		To commit changes, set @saveChanges = 1.
+ */
+declare @saveChanges bit; --set @saveChanges = 1
+
+declare @error int, @rowcount varchar(20)
+set nocount on; begin tran; save tran TX
+
+
+delete InsertUpdateRule_Now where ID=-6293
+delete BaseRule_Now where ID=-6293
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' current business rules deleted'
+
+delete InsertUpdateRule where ID=-6293
+delete BaseRule where ID=-6293
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' historical business rules deleted'
+
+delete AssetAudit where ID=-6293 and AssetType='BusinessRule'
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' AssetAudits deleted'
+
+
+if (@saveChanges = 1) goto OK
+raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
+ERR: rollback tran TX
+OK: commit
+DONE:
