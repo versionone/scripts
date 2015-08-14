cc8fd51c1b4047d920ab7e3e85d0c9b11a97f06b
rajiv <rajiv.delwadia@versionone.com>
2015-08-14 16:42:19 -0400

    update version info

--- purge-project/PurgeProject-15.2.sql
+++ purge-project/PurgeProject-15.2.sql
@@ -13,7 +13,7 @@ declare @scopeToPurge int; --set @scopeToPurge = 54198
 declare @allowRecursion bit; --set @allowRecursion = 1
 
 -- Ensure the correct database version
-declare @supportedVersion varchar(10); set @supportedVersion = '15.1'
+declare @supportedVersion varchar(10); set @supportedVersion = '15.2'
 if (@supportedVersion is not null) begin
 	if not exists (select * from SystemConfig where Name='Version' and Value like @supportedVersion + '.%') begin
 		raiserror('This script can only run on a %s VersionOne database',16,1, @supportedVersion)
