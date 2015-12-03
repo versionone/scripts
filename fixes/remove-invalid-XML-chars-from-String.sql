6bf87aaea47755d58c2283ce6c743bfac5718ca0
rajiv <rajiv.delwadia@versionone.com>
2015-12-03 14:30:29 -0500

    add cleansing nul-chars from string

--- fixes/remove-invalid-XML-chars-from-String.sql
+++ fixes/remove-invalid-XML-chars-from-String.sql
@@ -1,5 +1,5 @@
 /*	
- *	Remove Vertical Tab (ASCII 11) characters from String values.
+ *	Remove NUL characters (ASCII 0) and Vertical Tab (ASCII 11) characters from String values.
  *
  *	NOTE: This updates strings in situ!
  *
@@ -22,6 +22,20 @@ select @rowcount=@@ROWCOUNT, @error=@@ERROR
 if @error<>0 goto ERR
 raiserror('vertical-tab characters removed from %d strings', 0, 1, @rowcount) with nowait
 
+NULLOOP:
+update String set Value=cast(substring(HexValue, 1+0, N*2)+substring(HexValue, 1+(N+1)*2, datalength(HexValue)-(N+1)*2) as nvarchar(4000))
+from (
+	select String.ID, String.Value, HexValue=cast(String.Value as varbinary(max)), N=Counter.Value, C=substring(cast(String.Value as varbinary(max)), 1+Counter.Value*2, 2)
+	from String
+	join Counter on Counter.Value<len(String.Value)
+)_
+where C=0x0000 and String.ID=_.ID
+
+/* after every modifying statement, check for errors; optionally, emit status */
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+raiserror('NUL characters removed from %d strings', 0, 1, @rowcount) with nowait
+if @rowcount>0 goto NULLOOP
 
 if (@saveChanges = 1) begin raiserror('Committing changes', 0, 254); goto OK end
 raiserror('To commit changes, set @saveChanges=1',16,254)
