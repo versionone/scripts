0d190e665d84884ee3b286fbee649d57806dc959
codeEmitter <greg.johnson@versionone.com>
2016-05-19 11:13:28 -0400

    Add script to remove the registration date from the SystemConfig table.  Useful for when server migrations take place and the instance id changes, or any time registration needs to be forced.

---
+++ fixes/clear-registration-date.sql
@@ -0,0 +1,41 @@
+/*
+ *	Customer-facing SQL script template
+ *
+ *	NOTE:  This script defaults to rolling back changes.
+ *		To commit changes, set @saveChanges = 1.
+ */
+declare @saveChanges bit; --set @saveChanges = 1
+declare @supportedVersions varchar(1000); select @supportedVersions='15.*, 16.*'
+
+-- Ensure the correct database version
+BEGIN
+	declare @sep char(2); select @sep=', '
+	if not exists(select *
+		from dbo.SystemConfig
+		join (
+		select SUBSTRING(@supportedVersions, C.Value+1, CHARINDEX(@sep, @supportedVersions+@sep, C.Value+1)-C.Value-1) as Value
+		from dbo.Counter C
+		where C.Value < DataLength(@supportedVersions) and SUBSTRING(@sep+@supportedVersions, C.Value+1, DataLength(@sep)) = @sep
+		) Version on SystemConfig.Value like REPLACE(Version.Value, '*', '%') and SystemConfig.Name = 'Version'
+	) begin
+			raiserror('Only supported on version(s) %s',16,1, @supportedVersions)
+			goto DONE
+	end
+END
+
+declare @error int, @rowcount int
+set nocount on; begin tran; save tran TX
+
+DELETE FROM [dbo].SystemConfig
+WHERE [Name] = 'RegistrationDate';
+
+/* after every modifying statement, check for errors; optionally, emit status */
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+raiserror('%d rows deleted', 0, 1, @rowcount) with nowait
+
+if (@saveChanges = 1) begin raiserror('Committing changes', 0, 254); goto OK end
+raiserror('To commit changes, set @saveChanges=1',16,254)
+ERR: raiserror('Rolling back changes', 0, 255); rollback tran TX
+OK: commit
+DONE:
