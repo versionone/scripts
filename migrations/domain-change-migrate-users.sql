07a69233e337e11607d3f520ce1be39243defa84
Sean McCrohan <s.m.mccrohan@gmail.com>
2017-03-13 16:00:13 -0400

    Add 17 as a supported version; only touch users who had the expected old domain; don't assume all users had a domain in their name.

--- migrations/domain-change-migrate-users.sql
+++ migrations/domain-change-migrate-users.sql
@@ -4,9 +4,10 @@
  *	NOTE:  This script defaults to rolling back changes.
  *		To commit changes, set @saveChanges = 1.
  */
-declare @saveChanges bit; --set @saveChanges = 1
-declare @newDomain nvarchar(100); set @newDomain = 'your new domain' -- set this to your domain name
-declare @supportedVersions varchar(1000); select @supportedVersions='10.2.*, 10.3.*, 11.*, 15.*, 16.*'
+declare @saveChanges bit; set @saveChanges = 1
+declare @oldDomain nvarchar(100); set @oldDomain = 'Old Domain Name' -- set this to the old domain name that is being replaced
+declare @newDomain nvarchar(100); set @newDomain = 'New Domain Name' -- set this to your new domain name
+declare @supportedVersions varchar(1000); select @supportedVersions='10.2.*, 10.3.*, 11.*, 15.*, 16.*, 17.*'
 
 -- Ensure the correct database version
 BEGIN
@@ -27,25 +28,26 @@ END
 declare @error int, @rowcount int
 set nocount on; begin tran; save tran TX
 
-declare @Usernames table(ID int, originalUserName nvarchar(100), username nvarchar(100) collate Latin1_General_BIN2 not null)
+declare @Usernames table(ID int, originalUserName nvarchar(100), newUserName nvarchar(100) collate Latin1_General_BIN2 not null)
 INSERT @Usernames 
-SELECT ID, username as originalUserName, substring(username, charindex('\', username)+1, len(username)-1) as username
+SELECT ID, Username as originalUserName, 
+	@newDomain + '\' + substring(Username, charindex('\', Username)+1, len(Username)-1) as newUserName
 	FROM Login
+	WHERE Username like '%\%'
+	AND substring(Username, 0, charindex('\', Username)) = @oldDomain
 
 UPDATE Login
-	SET username = (
-		SELECT @newDomain + '\' + u.username
-		FROM @Usernames u
-		WHERE Login.ID=u.ID
-	)
+	SET Login.Username = u.newUserName
+	FROM @Usernames as u
+	WHERE Login.ID=u.ID
 
 select @rowcount=@@ROWCOUNT, @error=@@ERROR
 if @error<>0 goto ERR
 raiserror('%d Usernames updated', 0, 1, @rowcount) with nowait
 
-
 if (@saveChanges = 1) begin raiserror('Committing changes', 0, 254); goto OK end
 raiserror('To commit changes, set @saveChanges=1',16,254)
+
 ERR: raiserror('Rolling back changes', 0, 255); rollback tran TX
 OK: commit
 DONE:
