3cb14a3690e89779a17438ead6e758e515eb04ea
rajiv <rajiv.delwadia@versionone.com>
2017-05-04 12:45:42 -0400

    run each asset in its own transaction

--- redundant-history/remove-redundant-asset-history.sql
+++ redundant-history/remove-redundant-asset-history.sql
@@ -5,13 +5,71 @@
  *		To commit changes, set @saveChanges = 1.
  */
 declare @saveChanges bit; --set @saveChanges = 1
+declare @rows int = 0
 
-declare @error int, @rowcount varchar(20)
-set nocount on; begin tran; save tran TX
+declare @sqlTemplate varchar(max); select @sqlTemplate = '
+	declare @error int, @rowcount int
+	begin tran; save tran TX2
 
-create table #results (Asset sysname not null, [rowcount] int not null, error int not null)
-declare @now sysname, @hist sysname, @cols varchar(max), @colsEqual varchar(max)
-declare C cursor local fast_forward for
+	;with H as (
+		select ID, AuditBegin, R=ROW_NUMBER() OVER(partition by ID order by AuditBegin)
+			{cols}
+		from dbo.[{hist}]
+	)
+	delete dbo.[{hist}]
+	from H A
+	join H B on A.ID=B.ID and A.R+1=B.R
+	where [{hist}].ID=B.ID and [{hist}].AuditBegin=B.AuditBegin
+		{colsEqual}
+
+	select @rowcount=@@ROWCOUNT, @error=@@ERROR
+	if @error<>0 goto ERR
+		
+	if @rowcount>0 begin
+		select @rows = @rows + @rowcount
+		raiserror(''%d {hist} historical records deleted'', 0, 1, @rowcount) with nowait
+
+		;with H as (
+			select ID, AuditBegin, AuditEnd, R=ROW_NUMBER() over(partition by ID order by AuditBegin)
+			from dbo.[{hist}]
+		)
+		update dbo.[{hist}] set AuditEnd=B.AuditBegin
+		from H A
+		left join H B on A.ID=B.ID and A.R+1=B.R
+		where [{hist}].ID=A.ID and [{hist}].AuditBegin=A.AuditBegin
+			and isnull(A.AuditEnd,-1)<>isnull(B.AuditBegin,-1)
+
+		select @rowcount=@@ROWCOUNT, @error=@@ERROR
+		if @error<>0 goto ERR
+		raiserror(''%d {hist} historical records stitched'', 0, 2, @rowcount) with nowait
+				
+		alter table dbo.[{now}] disable trigger all
+
+		update dbo.[{now}] set AuditBegin=[{hist}].AuditBegin 
+		from dbo.[{hist}]
+		where [{hist}].ID=[{now}].ID and [{hist}].AuditEnd is null and [{hist}].AuditBegin<>[{now}].AuditBegin
+
+		select @rowcount=@@ROWCOUNT, @error=@@ERROR
+		alter table dbo.[{now}] enable trigger all
+
+		if @error<>0 goto ERR
+		raiserror(''%d {now} records synced'', 0, 3, @rowcount) with nowait
+
+		insert #sql values(''raiserror(''''reindex {hist}'''', 0, 4) with nowait; dbcc dbreindex (''''dbo.[{hist}]'''')'')
+
+		if ({saveChanges} = 1) goto OK
+		raiserror(''To commit changes, set @saveChanges=1'',16,254) with nowait
+		ERR: raiserror(''Rolling back changes'', 0, 255) with nowait; rollback tran TX2
+	end
+	OK: commit
+'
+
+set nocount on
+create table #sql (sql varchar(max) not null)
+
+insert #sql
+select REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(@sqlTemplate, '{now}', nowTable), '{hist}', histTable), '{colsEqual}', colsEqual), '{cols}', cols), '{saveChanges}', isnull(@saveChanges, 0))
+from (
 	select N.TABLE_NAME nowTable, H.TABLE_NAME histTable, 
 		cols = 
 			(
@@ -42,79 +100,20 @@ declare C cursor local fast_forward for
 			-- Skip "system" assets
 			'Role', 'State'
 		)
+)_
 
+declare @sql varchar(max)
+declare C cursor local fast_forward for select sql from #sql
 open C
 while 1=1 begin
-	fetch next from C into @now, @hist, @cols, @colsEqual
+	fetch next from C into @sql
 	if @@FETCH_STATUS<>0 break
-
-	declare @sql varchar(max); select @sql = '
-		declare @error int, @rowcount int
-
-		;with H as (
-			select ID, AuditBegin, R=ROW_NUMBER() OVER(partition by ID order by AuditBegin)
-				{cols}
-			from dbo.[{hist}]
-		)
-		delete dbo.[{hist}]
-		from H A
-		join H B on A.ID=B.ID and A.R+1=B.R
-		where [{hist}].ID=B.ID and [{hist}].AuditBegin=B.AuditBegin
-		 {colsEqual}
-
-		select @rowcount=@@ROWCOUNT, @error=@@ERROR
-		insert #results values(''{hist}'', @rowcount, @error)
-		
-		if @rowcount>0 begin
-			;with H as (
-				select ID, AuditBegin, AuditEnd, R=ROW_NUMBER() over(partition by ID order by AuditBegin)
-				from dbo.[{hist}]
-			)
-			update dbo.[{hist}] set AuditEnd=B.AuditBegin
-			from H A
-			left join H B on A.ID=B.ID and A.R+1=B.R
-			where [{hist}].ID=A.ID and [{hist}].AuditBegin=A.AuditBegin
-				and isnull(A.AuditEnd,-1)<>isnull(B.AuditBegin,-1)
-				
-			alter table dbo.[{now}] disable trigger all
-
-			update dbo.[{now}] set AuditBegin=[{hist}].AuditBegin 
-			from dbo.[{hist}]
-			where [{hist}].ID=[{now}].ID and [{hist}].AuditEnd is null and [{hist}].AuditBegin<>[{now}].AuditBegin
-
-			alter table dbo.[{now}] enable trigger all
-		end'
-		
-	select @sql = REPLACE(@sql, '{now}', @now)
-	select @sql = REPLACE(@sql, '{hist}', @hist)
-	select @sql = REPLACE(@sql, '{colsEqual}', @colsEqual)
-	select @sql = REPLACE(@sql, '{cols}', @cols)
 
 	--print(@sql)
 	exec(@sql)
-	
-	select @error=error, @rowcount=[rowcount] from #results where Asset=@hist
-	if @error<>0 break
-	if @rowcount>0 begin
-		print @rowcount + ' ' + @hist + ' historical records consolidated'
-
-		if @saveChanges=1 exec('DBCC DBREINDEX([' + @hist + '])')
-	end
 end
 
 close C; deallocate C
+--select * from #sql
 
-/* after every modifying statement, check for errors; optionally, emit status */
-select @rowcount=sum([rowcount]), @error=sum(error) from #results
-select * from #results
-drop table #results
-
-if @error<>0 goto ERR
-print '-------------'
-print @rowcount + ' total records consolidated'
-
-if (@saveChanges = 1) goto OK
-raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
-ERR: rollback tran TX
-OK: commit
-DONE:
+drop table #sql
