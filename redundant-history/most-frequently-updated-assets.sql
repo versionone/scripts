6d7304d48e1c2ab48da423e09417d05403bc7356
Matt Higgins <mhhiggins@gmail.com>
2019-04-29 13:31:45 -0400

    apply a more realistic "normal" upper bound

--- redundant-history/most-frequently-updated-assets.sql
+++ redundant-history/most-frequently-updated-assets.sql
@@ -1,6 +1,6 @@
 select ID, AssetType, Cnt from
 (
 	select *, ROW_NUMBER() over(partition by AssetType order by Cnt desc) r
-	from (select count(1) Cnt, ID, AssetType from dbo.AssetAudit group by ID, AssetType having count(1) > 20) _
+	from (select count(1) Cnt, ID, AssetType from dbo.AssetAudit group by ID, AssetType having count(1) > 50) _
 ) __
 where r <= 100
