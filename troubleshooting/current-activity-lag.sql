4215054b0f38093b5349cb57875a59e6536957bf
Pavel Mamut <pavel@collab.net>
2019-10-31 12:21:48 -0400

    added `Now` to the output to facilitate monitoring

--- troubleshooting/current-activity-lag.sql
+++ troubleshooting/current-activity-lag.sql
@@ -1,13 +1,14 @@
 ;with M as (
-	select MaxSequence=convert(int, ltrim(rtrim(substring(Value, patindex('%"MaxSequence":%', Value)+14, patindex('%'+CHAR(13)+char(10)+'}%', Value)-patindex('%"MaxSequence": %', Value)-14))))
+	select Now=GETUTCDATE(), MaxSequence=convert(int, ltrim(rtrim(substring(Value, patindex('%"MaxSequence":%', Value)+14, patindex('%'+CHAR(13)+char(10)+'}%', Value)-patindex('%"MaxSequence": %', Value)-14))))
 	from Config 
 	where Instance='MetaStreamSubscription' and Type='CatchupSubscriptionManager'
 ),
 A as (select top 1 * from Commits order by CommitSequence desc),
-B as (select Commits.* from Commits join M on M.MaxSequence=CommitSequence)
+B as (select Commits.*, Now from Commits join M on M.MaxSequence=CommitSequence)
 select 
+	Now,
 	Latest=A.CommitStamp, 
 	Processed=B.CommitStamp, 
 	Behind_Sec=DATEDIFF(s, B.CommitStamp, A.CommitStamp),
-	Lag_Sec=DATEDIFF(s, B.CommitStamp, GETUTCDATE())
+	Lag_Sec=DATEDIFF(s, B.CommitStamp, Now)
 from A,B
