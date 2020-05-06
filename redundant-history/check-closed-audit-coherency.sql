0ee39af533d0dd3b2596206040597e05f7da6254
Pavel Mamut <pavel@collab.net>
2020-05-05 22:36:20 -0400

    ClosedAuditID coherency check and clean-up scripts
    S-70222

---
+++ redundant-history/check-closed-audit-coherency.sql
@@ -0,0 +1,40 @@
+set nocount on
+
+declare @cnt int, @total int
+select @total=0
+
+select @cnt=count(*) from dbo.BaseAsset_Now where ClosedAuditID>AuditBegin
+select @total=@total+@cnt
+raiserror('%d BaseAsset_Now with a future closed audit', 0, 1, @cnt) with nowait
+
+select @cnt=count(*) from dbo.BaseAsset_Now where ClosedAuditID is not null and AssetState<128
+select @total=@total+@cnt
+raiserror('%d open BaseAsset_Now with a closed audit', 0, 1, @cnt) with nowait
+
+select @cnt=count(*) from dbo.BaseAsset where ClosedAuditID is not null and AssetState<128
+select @total=@total+@cnt
+raiserror('%d open BaseAsset with a closed audit', 0, 1, @cnt) with nowait
+
+select @cnt=count(*)
+from dbo.BaseAsset_Now ban
+join dbo.BaseAsset ba on ba.ID=ban.ID and ba.AuditEnd is null
+and ((ba.ClosedAuditID is null and ban.ClosedAuditID is not null) or (ba.ClosedAuditID is not null and ban.ClosedAuditID is null) or ba.ClosedAuditID<>ban.ClosedAuditID)
+select @total=@total+@cnt
+raiserror('%d closed audit mismatch betwen BaseAsset_Now and BaseAsset tip', 0, 1, @cnt) with nowait
+
+select @cnt=count(*)
+from dbo.BaseAsset ba
+cross apply (select top 1 bac.* from BaseAsset bac where bac.ID=ba.ID and bac.AssetState=128 and bac.AuditBegin<=ba.AuditBegin and not exists (select * from BaseAsset bao where bao.ID=bac.ID and bao.AssetState<128 and bao.AuditBegin>bac.AuditBegin and bao.AuditBegin<ba.AuditBegin) order by AuditBegin) bac
+where ba.AssetState>=128 and ba.ClosedAuditID<>bac.AuditBegin
+select @total=@total+@cnt
+raiserror('%d closed audit mismatch between closed/dead BasseAsset and its most recent closure', 0, 1, @cnt) with nowait
+
+select @cnt=count(*)
+from dbo.BaseAsset ba
+outer apply (select top 1 bac.* from BaseAsset bac where bac.ID=ba.ID and bac.AssetState=128 and bac.AuditBegin<ba.AuditBegin and not exists (select * from BaseAsset bao where bao.ID=bac.ID and bao.AssetState<128 and bao.AuditBegin>bac.AuditBegin and bao.AuditBegin<ba.AuditBegin) order by AuditBegin) bac
+where ba.AssetState>=192 and ba.ClosedAuditID is not null and bac.AuditBegin is null
+select @total=@total+@cnt
+raiserror('%d dead never closed BaseAsset with a closed audit', 0, 1, @cnt) with nowait
+
+if @total>0 raiserror('%d incoherent closed audits detected', 16, 1, @total)
+	
