c1d1472b182ecf9f363aaacea4fa8e44c8b6b6eb
Jim Allers <jim.allers@gmail.com>
2020-07-01 09:20:42 -0400

    Path node before IconProxy varies by deployment

--- troubleshooting/html-encoded-descriptions.sql
+++ troubleshooting/html-encoded-descriptions.sql
@@ -2,4 +2,4 @@
 select ba.ID, strs.Value, ls.Value from dbo.LongString ls
 inner join dbo.BaseAsset_Now ba on ba.Description = ls.ID
 inner join dbo.String strs on ba.Name = strs.ID
-where ls.Value like '%&lt;img class="asset-icon" src="/VersionOne.Web/IconProxy.mvc/Resolve?assetType=Member%';
+where ls.Value like '%&lt;img class="asset-icon" src="/%/IconProxy.mvc/Resolve?assetType=Member%';
