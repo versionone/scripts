e9cce37cac72fc9cfd365aad24ca43e48a165f17
Laureano Remedi <lremedi@gmail.com>
2021-03-02 16:19:29 -0500

    S-74207: Script to consolidate multiple, alike Description history records. Removing LEAD/LAG funtions, going with similar approach than 'open/close/reopen' consolitation script.

--- fixes/consolidate-multiple-description-history.sql
+++ fixes/consolidate-multiple-description-history.sql
@@ -1,102 +1,150 @@
-/*	
- *	Consolidates lDescription field history records.
- *	
+/*
+ *	Consolidate description field history records, on configurable time threshold (@timeThreshold) in minutes.
+ *
  *	NOTE:  This script defaults to rolling back changes.
  *		To commit changes, set @saveChanges = 1.
  */
-declare @saveChanges bit; --set @saveChanges = 1
-declare @error int, @rowcount int
+declare @saveChanges bit; -- set @saveChanges = 1
+declare @timeThreshold int;
+
+set @timeThreshold = 5
+
+
+create table #baseassets(ID int not null, PreviousAuditID int null, CurrentAuditID int null, NextAuditId int null, [NextChangedByID] int not null, [CurrentChangedByID] int not null, [NextChangeDateUTC] datetime not null,[CurrentChangeDateUTC] datetime not null, [PreviousDescription] int null, [CurrentDescription] int null, [NextDescription] int null)
+create table #suspect(ID int not null, PreviousAuditID int null, CurrentAuditID int null, NextAuditId int null)
+create table #bad(ID int not null, PreviousAuditID int null, CurrentAuditID int null, NextAuditId int null)
+
 set nocount on; begin tran; save tran TX
 
-DECLARE @toRemove TABLE(
-	[ID] [int] NOT NULL,
-	[AssetType] [varchar](100) COLLATE Latin1_General_BIN NOT NULL,
-	[AuditBegin] [int] NOT NULL)  
-INSERT INTO @toRemove
-SELECT Main.ID, Main.AssetType, Main.AuditBegin
-FROM
-( SELECT BA.ID, BA.[AssetType],BA.[AuditBegin],BA.[AuditEnd],BA.[Description], A.ChangedByID, A.[ChangeDateUTC]
-  ,LEAD([ChangedByID]) OVER (ORDER BY A.[ChangeDateUTC]) AS NextUserID
-  FROM [BaseAsset] BA
-  JOIN [Audit] A ON A.ID = BA.AuditBegin 
-) AS Main
-LEFT JOIN (
-	SELECT *
-	FROM (
-	SELECT BA.ID,BA.[AssetType],BA.[AuditBegin],BA.[AuditEnd],BA.[Description],A.[ChangeDateUTC],A.[ChangedByID]
-		  ,LEAD([ChangedByID]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS NextUserID
-		  ,LEAD([ChangeDateUTC]) OVER (PARTITION BY BA.ID, BA.AssetType, [ChangedByID] ORDER BY A.[ChangeDateUTC]) AS NextChangeUTC
-		  ,LEAD(BA.[Description]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS NextDescriptionID
-		  ,LAG(BA.[Description]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS PreviousDescriptionID
-	  FROM [BaseAsset] BA
-	  JOIN [Audit] A ON A.ID = BA.AuditBegin) AS BaseQuery
-	WHERE 
-	ISNULL(NextUserID,-1) = [ChangedByID] -- Consecutive changes from the same user
-	AND 
-	DATEDIFF(mi,[ChangeDateUTC],NextChangeUTC) < 5 --Time threshold / period / lapse.
-	AND (
-		ISNULL(NextDescriptionID,-1) != ISNULL([Description],-1) --Description has changed
-		AND
-		ISNULL(PreviousDescriptionID,-1) != ISNULL([Description],-1) --Description has changed
-	)
-) AS ToRemove on Main.AuditBegin = ToRemove.AuditBegin AND Main.ID = ToRemove.ID
-WHERE NOT ToRemove.AuditBegin IS NULL
-ORDER BY ID, AssetType,Main.AuditBegin
-update dbo.[BaseAsset]
-set AuditEnd=NewAuditEnd
-FROM (
-SELECT *
-FROM (
-	SELECT Main.ID, Main.AssetType, Main.AuditBegin, Main.AuditEnd, LEAD(Main.AuditBegin) OVER (PARTITION BY Main.ID, Main.AssetType ORDER BY Main.ChangeDateUTC) NewAuditEnd
-	FROM
-	( SELECT BA.ID, BA.[AssetType],BA.[AuditBegin],BA.[AuditEnd],BA.[Description], A.ChangedByID, A.[ChangeDateUTC]
-	  ,LEAD([ChangedByID]) OVER (ORDER BY A.[ChangeDateUTC]) AS NextUserID
-	  FROM [BaseAsset] BA
-	  JOIN [Audit] A ON A.ID = BA.AuditBegin 
-	) AS Main
-	LEFT JOIN (
-		SELECT *
-		FROM (
-		SELECT BA.ID,BA.[AssetType],BA.[AuditBegin],BA.[AuditEnd],BA.[Description],A.[ChangeDateUTC],A.[ChangedByID]
-			  ,LEAD([ChangedByID]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS NextUserID
-			  ,LEAD([ChangeDateUTC]) OVER (PARTITION BY BA.ID, BA.AssetType, [ChangedByID] ORDER BY A.[ChangeDateUTC]) AS NextChangeUTC
-			  ,LEAD(BA.[Description]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS NextDescriptionID
-			  ,LAG(BA.[Description]) OVER (PARTITION BY BA.ID, BA.AssetType ORDER BY A.[ChangeDateUTC]) AS PreviousDescriptionID
-		  FROM [BaseAsset] BA
-		  JOIN [Audit] A ON A.ID = BA.AuditBegin) AS BaseQuery
-		WHERE 
-		ISNULL(NextUserID,-1) = [ChangedByID] -- Consecutive changes from the same user
-		AND 
-		DATEDIFF(mi,[ChangeDateUTC],NextChangeUTC) < 5 --Time threshold / period / lapse.
-		AND (
-			ISNULL(NextDescriptionID,-1) != ISNULL([Description],-1) --Description has changed
-			AND
-			ISNULL(PreviousDescriptionID,-1) != ISNULL([Description],-1)
-		)
-	) AS ToRemove on Main.AuditBegin = ToRemove.AuditBegin AND Main.ID = ToRemove.ID
-	WHERE ToRemove.AuditBegin IS NULL) as ToUpdate
-WHERE 
-NOT NewAuditEnd IS NULL
-AND AuditEnd != NewAuditEnd
-) AS Core
-WHERE 
-[BaseAsset].ID = Core.ID 
-AND [BaseAsset].AssetType = Core.AssetType 
-AND [BaseAsset].[AuditBegin] = Core.[AuditBegin]
-AND Core.AuditEnd != Core.NewAuditEnd 
-DELETE BA
-FROM [BaseAsset] BA
-JOIN @toRemove TR ON BA.ID = TR.ID AND BA.AssetType = TR.AssetType AND BA.AuditBegin = TR.AuditBegin
-DELETE AA
-FROM [AssetAudit] AA
-JOIN @toRemove TR ON AA.ID = TR.ID AND AA.AssetType = TR.AssetType AND AA.AuditID = TR.AuditBegin
-DELETE A
-FROM [Audit] A
-JOIN @toRemove TR ON A.ID = TR.AuditBegin
+-- consecutive BaseAsset changes
+;with H as (
+	select AA.ID, AssetType, AuditID, A.[ChangedByID], A.ChangeDateUTC ,R=ROW_NUMBER() OVER(partition by AA.ID, AssetType order by AuditID)
+	from dbo.AssetAudit AA
+	join [Audit] A ON A.ID = AA.AuditID
+)
+
+insert #baseassets(ID, PreviousAuditID, CurrentAuditID, NextAuditID, [NextChangedByID], [CurrentChangedByID], [NextChangeDateUTC], [CurrentChangeDateUTC], [PreviousDescription], [CurrentDescription], [NextDescription])
+select A.ID,
+C.AuditID PreviousAuditID,
+A.AuditID CurrentAuditID,
+B.AuditID NextAuditID,
+B.[ChangedByID] AS [NextChangedByID],
+A.[ChangedByID] AS [CurrentChangedByID],
+B.[ChangeDateUTC] AS [NextChangeDateUTC],
+A.[ChangeDateUTC] AS [CurrentChangeDateUTC],
+bC.[Description] AS [PreviousDescription],
+bA.[Description] AS [CurrentDescription],
+bB.[Description] AS [NextDescription]
+from H A
+join dbo.BaseAsset bA on bA.ID=A.ID and bA.AssetType=A.AssetType and bA.AuditBegin=A.AuditID
+
+join H B on B.ID=A.ID and B.AssetType=A.AssetType and B.R=A.R+1
+join dbo.BaseAsset bB on bB.ID=B.ID and bB.AssetType=B.AssetType and bB.AuditBegin=B.AuditID
+
+left join H C on C.ID=B.ID and C.AssetType=B.AssetType and C.R=A.R-1
+left join dbo.BaseAsset bC on bC.ID=C.ID and bC.AssetType=C.AssetType and bC.AuditBegin=C.AuditID
 
 
-if (@saveChanges = 1) begin raiserror('Committing changes', 0, 254); goto OK end
-raiserror('To commit changes, set @saveChanges=1',16,254)
-ERR: raiserror('Rolling back changes', 0, 255); rollback tran TX
+insert #suspect(ID,  PreviousAuditID, CurrentAuditID, NextAuditID)
+select _.ID,
+PreviousAuditID,
+CurrentAuditID,
+NextAuditID
+from #baseassets _
+WHERE 1=1
+AND ISNULL([NextChangedByID] ,-1) = [CurrentChangedByID]  -- Consecutive changes from the same user
+AND
+DATEDIFF(mi,[CurrentChangeDateUTC],[NextChangeDateUTC]) < @timethreshold --Time threshold / period / lapse.
+AND
+(
+	ISNULL([PreviousDescription],-1) != ISNULL([CurrentDescription],-1) --Description has changed
+	AND
+	ISNULL([NextDescription],-1) != ISNULL([CurrentDescription],-1) --Description has changed
+)
+
+-- BaseAsset column comparison
+declare @colsAB varchar(max), @colsBC varchar(max)
+select @colsAB=(
+	select REPLACE(' and (A.{col}=B.{col} or (A.{col} is null and B.{col} is null))', '{col}', quotename(COLUMN_NAME))
+	from INFORMATION_SCHEMA.COLUMNS C
+	where C.TABLE_NAME='BaseAsset_Now' and COLUMN_NAME not in ('ID','AssetType','AuditBegin', 'Description', 'ClosedAuditID')
+	for xml path('')
+)
+select @colsBC=(
+	select REPLACE(' and (B.{col}=C.{col} or (B.{col} is null and C.{col} is null))', '{col}', quotename(COLUMN_NAME))
+	from INFORMATION_SCHEMA.COLUMNS C
+	where C.TABLE_NAME='BaseAsset_Now' and COLUMN_NAME not in ('ID','AssetType','AuditBegin', 'Description', 'ClosedAuditID')
+	for xml path('')
+)
+
+-- consecutive redundant BaseAsset re-open/close
+declare @sql varchar(max); select @sql = '
+insert #bad(ID, PreviousAuditID, CurrentAuditID, NextAuditID)
+select _.ID, PreviousAuditID, CurrentAuditID, NextAuditID
+from #suspect _
+join dbo.BaseAsset A on A.ID=_.ID and A.AuditBegin=_.PreviousAuditID
+join dbo.BaseAsset B on B.ID=_.ID and B.AuditBegin=_.CurrentAuditID
+' + @colsAB + '
+join dbo.BaseAsset C on C.ID=_.ID and C.AuditBegin=_.NextAuditID
+' + @colsBC
+
+-- print @sql
+exec(@sql)
+
+-- purge redundant rows
+delete dbo.BaseAsset
+from #bad
+where BaseAsset.ID=#bad.ID and (BaseAsset.AuditBegin=#bad.CurrentAuditID)
+
+declare @error int, @rowcount varchar(20)
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' BaseAsset description historical records consolidated'
+
+if @rowcount=0 goto FINISHED
+
+-- re-stitch BaseAsset history
+;with H as (
+	select ID, AuditBegin, AuditEnd, R=ROW_NUMBER() over(partition by ID order by AuditBegin)
+	from dbo.BaseAsset
+)
+update dbo.BaseAsset set AuditEnd=B.AuditBegin
+from H A
+left join H B on A.ID=B.ID and A.R+1=B.R
+where BaseAsset.ID=A.ID and BaseAsset.AuditBegin=A.AuditBegin
+	and isnull(A.AuditEnd,-1)<>isnull(B.AuditBegin,-1)
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+if @error<>0 goto ERR
+print @rowcount + ' BaseAsset history records restitched'
+
+alter table dbo.BaseAsset_Now disable trigger all
+
+-- sync up BaseAsset_Now with history tips from BaseAsset
+update dbo.BaseAsset_Now set AuditBegin=BaseAsset.AuditBegin, ClosedAuditID=BaseAsset.ClosedAuditID
+from dbo.BaseAsset
+where BaseAsset.ID=BaseAsset_Now.ID and BaseAsset.AuditEnd is null and BaseAsset.AuditBegin<>BaseAsset_Now.AuditBegin
+
+select @rowcount=@@ROWCOUNT, @error=@@ERROR
+alter table dbo.BaseAsset_Now enable trigger all
+if @error<>0 goto ERR
+print @rowcount + ' BaseAsset_Now records syncd'
+
+if @saveChanges=1 begin
+	DBCC DBREINDEX([BaseAsset])
+
+	exec dbo.AssetAudit_Rebuild
+	DBCC DBREINDEX([AssetAudit])
+end
+
+FINISHED:
+if (@saveChanges = 1) goto OK
+raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
+ERR: rollback tran TX
 OK: commit
 DONE:
+
+drop table #bad
+drop table #suspect
+drop table #baseassets
