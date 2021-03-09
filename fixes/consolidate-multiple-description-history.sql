5c1cb7fca85834e40c9d0155e4464019ad680489
Laureano Remedi <lremedi@gmail.com>
2021-03-09 11:37:33 -0500

    S-74207 Not removing non relevant rows.

--- fixes/consolidate-multiple-description-history.sql
+++ fixes/consolidate-multiple-description-history.sql
@@ -9,41 +9,28 @@ declare @timeThreshold int;
 
 set @timeThreshold = 5
 
-create table #suspect(ID int not null, PreviousAuditID int null, CurrentAuditID int null, NextAuditId int null)
-create table #bad(ID int not null, PreviousAuditID int null, CurrentAuditID int null, NextAuditId int null)
+create table #suspect(ID int not null, CurrentAuditID int null, NextAuditId int null)
+create table #bad(ID int not null, CurrentAuditID int null, NextAuditId int null)
 
 set nocount on; begin tran; save tran TX
 
 -- consecutive BaseAsset changes
 ;with H as (
-	select AA.ID, AssetType, AuditID, A.[ChangedByID], A.ChangeDateUTC ,R=ROW_NUMBER() OVER(partition by AA.ID, AssetType order by AuditID)
+	select AA.ID, AA.AssetType, AuditID, A.[ChangedByID], A.ChangeDateUTC, bA.[Description] ,R=ROW_NUMBER() OVER(partition by AA.ID, AA.AssetType order by AuditID)
 	from dbo.AssetAudit AA
 	join [Audit] A ON A.ID = AA.AuditID
+	join dbo.BaseAsset bA on bA.ID=AA.ID and bA.AssetType=AA.AssetType and bA.AuditBegin=AA.AuditID
 )
 
-insert #suspect(ID,  PreviousAuditID, CurrentAuditID, NextAuditID)
+insert #suspect(ID, CurrentAuditID, NextAuditID)
 select A.ID,
-C.AuditID PreviousAuditID,
 A.AuditID CurrentAuditID,
 B.AuditID NextAuditID
-from H A
-join dbo.BaseAsset bA on bA.ID=A.ID and bA.AssetType=A.AssetType and bA.AuditBegin=A.AuditID
-
-join H B on B.ID=A.ID and B.AssetType=A.AssetType and B.R=A.R+1
-join dbo.BaseAsset bB on bB.ID=B.ID and bB.AssetType=B.AssetType and bB.AuditBegin=B.AuditID
-
-left join H C on C.ID=B.ID and C.AssetType=B.AssetType and C.R=A.R-1
-left join dbo.BaseAsset bC on bC.ID=C.ID and bC.AssetType=C.AssetType and bC.AuditBegin=C.AuditID
-
-WHERE 1=1
+FROM H as A
+JOIN H as B on B.ID=A.ID and B.AssetType=A.AssetType and B.R=A.R+1
 AND ISNULL(B.[ChangedByID] ,-1) = A.[ChangedByID]  -- Consecutive changes from the same user
-AND DATEDIFF(mi,A.[ChangeDateUTC] ,B.[ChangeDateUTC]) < @timethreshold --Time threshold / period / lapse.
-AND
-(
-	ISNULL(bC.[Description],-1) != ISNULL(bA.[Description],-1) --Description has changed
-	AND
-	ISNULL(bB.[Description],-1) != ISNULL(bA.[Description],-1) --Description has changed
-)
+AND DATEDIFF(mi,A.[ChangeDateUTC] ,B.[ChangeDateUTC]) <= @timethreshold --Time threshold / period / lapse.
+AND	ISNULL(B.[Description],-1) != ISNULL(A.[Description],-1) --Description has changed
 
 -- BaseAsset column comparison
 declare @colsAB varchar(max), @colsBC varchar(max)
@@ -53,23 +40,15 @@ select @colsAB=(
 	where C.TABLE_NAME='BaseAsset' and COLUMN_NAME not in ('ID','AssetType','AuditBegin', 'Description', 'AuditEnd')
 	for xml path('')
 )
-select @colsBC=(
-	select REPLACE(' and (B.{col}=C.{col} or (B.{col} is null and C.{col} is null))', '{col}', quotename(COLUMN_NAME))
-	from INFORMATION_SCHEMA.COLUMNS C
-	where C.TABLE_NAME='BaseAsset' and COLUMN_NAME not in ('ID','AssetType','AuditBegin', 'Description', 'AuditEnd')
-	for xml path('')
-)
 
 -- consecutive redundant BaseAsset re-open/close
 declare @sql varchar(max); select @sql = '
-insert #bad(ID, PreviousAuditID, CurrentAuditID, NextAuditID)
-select _.ID, PreviousAuditID, CurrentAuditID, NextAuditID
+insert #bad(ID, CurrentAuditID, NextAuditID)
+select _.ID, CurrentAuditID, NextAuditID
 from #suspect _
-join dbo.BaseAsset A on A.ID=_.ID and A.AuditBegin=_.PreviousAuditID
-join dbo.BaseAsset B on B.ID=_.ID and B.AuditBegin=_.CurrentAuditID
-' + @colsAB + '
-join dbo.BaseAsset C on C.ID=_.ID and C.AuditBegin=_.NextAuditID
-' + @colsBC
+join dbo.BaseAsset A on A.ID=_.ID and A.AuditBegin=_.CurrentAuditID
+join dbo.BaseAsset B on B.ID=_.ID and B.AuditBegin=_.NextAuditID
+' + @colsAB
 
 -- print @sql
 exec(@sql)
