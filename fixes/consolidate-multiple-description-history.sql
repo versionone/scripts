1da22067a820ac44ecd2b02f7d8378668853d93d
Laureano Remedi <lremedi@gmail.com>
2021-03-25 12:00:25 -0400

    D-17750 dbo missing

--- fixes/consolidate-multiple-description-history.sql
+++ fixes/consolidate-multiple-description-history.sql
@@ -64,7 +64,7 @@ exec(@sql)
 -- rows to purge
 select ba.ID, ba.AssetType, ba.AuditBegin, a.ChangedByID, a.ChangeDateUTC
 from #bad b
-join BaseAsset ba ON ba.ID=b.ID and ba.AuditBegin=b.CurrentAuditID
+join dbo.BaseAsset ba ON ba.ID=b.ID and ba.AuditBegin=b.CurrentAuditID
 join dbo.[Audit] a on a.ID = b.CurrentAuditID
 
 -- purge redundant rows
