41bb29d398108bad25daf64b24a1a15c9ac40f13
Laureano Remedi <laureano.remedi@digital.ai>
2023-10-16 10:58:36 -0300

    Changes after code review:  - renamed Behind to CycleTime  - added Behind as a difference between latest audit and latest commit on table  - using utc datetime to get Lag

--- troubleshooting/current-commit-lag.sql
+++ troubleshooting/current-commit-lag.sql
@@ -2,10 +2,12 @@
 	select top 1 cast(json_value(stuff(cast(Payload as varchar(max)), 1, 3, ''), '$[0].Body.Timestamp') as datetime) AuditStamp, cast(CommitStamp as datetime) CommitStamp
 	from dbo.Commits
 	order by CheckpointNumber desc
-)
+),
+A as (select top 1 * from dbo.Audit order by ID desc)
 select
-Latest = AuditStamp,
+Ocurred = AuditStamp,
 Processed = CommitStamp,
-Behind = CONVERT(FLOAT,CommitStamp-AuditStamp) * 24 * 60, --Decimal Minutes
-Lag = CONVERT(FLOAT,getdate()-CommitStamp) * 24 * 60 --Decimal Minutes
-from stamps
+CycleTime_Min = CONVERT(FLOAT,CommitStamp - AuditStamp) * 24 * 60, --Decimal Minutes
+Behind_Min = CONVERT(FLOAT,A.ChangeDateUTC - AuditStamp) * 24 * 60, --Decimal Minutes
+Lag_Min = CONVERT(FLOAT,getutcdate()-CommitStamp) * 24 * 60 --Decimal Minutes
+from stamps, A
