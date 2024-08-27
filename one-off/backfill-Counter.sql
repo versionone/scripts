0005821ed321253d97cd5c81cc22eb3c5bd4f3ba
lremedi <lremedi@digital.ai>
2024-08-27 18:47:58 -0300

    Backfill Counter table from 8000 to 16000

---
+++ one-off/backfill-Counter.sql
@@ -0,0 +1,18 @@
+ /*
+ *	NOTE:  This script defaults to rolling back changes.
+ *		To commit changes, set @saveChanges = 1.
+ */
+declare @saveChanges bit; --set @saveChanges = 1
+if (select count(*) from dbo.[Counter])=8000
+begin
+	begin tran; save tran TX
+	insert into dbo.Counter (Value)
+	select Value + 8000 from .dbo.Counter;
+
+	if @@ERROR<>0 goto ERR
+
+	if (@saveChanges = 1) goto OK
+	raiserror('Rolling back changes.  To commit changes, set @saveChanges=1',16,1)
+	ERR: rollback tran TX
+	OK: commit
+end
